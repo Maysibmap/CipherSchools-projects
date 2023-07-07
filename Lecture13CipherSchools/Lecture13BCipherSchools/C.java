277cdab85b5e14889d97cd061b2f8868060694a0
Mayank verma <112979691+Maysibmap@users.noreply.github.com>
2023-07-07 16:25:55 +0530

    first commit

---
+++ Lecture13CipherSchools/Lecture13BCipherSchools/C.java
@@ -0,0 +1,12 @@
+package Lecture13CipherSchools.Lecture13BCipherSchools;
+import Lecture13CipherSchools.Lecture13ACipherSchools.A;
+public class C extends A{
+    public static void main(String[] args) {
+        A obj=new A();
+        C obj2=new C();
+        // System.out.println(obj.x) default not accessible outside the package for subclass
+        // System.out.println(obj.z) private not accessible outside class
+        System.out.println(obj.d); // public is accessible everywhere 
+        System.out.println(obj2.y); // Protected is accessible for different package for subclass using object of subclass only. 
+    }
+}
---
+++ Lecture13CipherSchools/Lecture13BCipherSchools/D.java
@@ -0,0 +1,11 @@
+package Lecture13CipherSchools.Lecture13BCipherSchools;
+import Lecture13CipherSchools.Lecture13ACipherSchools.A;
+public class D {
+    public static void main(String[] args) {
+        A obj=new A();
+        // System.out.println(obj.x); default is not accessible outside the package
+        // System.out.println(obj.y); protected is not accessible outside the class
+        // System.out.println(obj.z); private is not accessible outside the package
+        System.out.println(obj.d);// Public is accessible outside the package
+    }
+}
